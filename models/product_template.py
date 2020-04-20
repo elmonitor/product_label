0ad2e9fc979b976020bf1adeed7bee527ce488e3
elmonitor <info@elmonitor.net>
2020-04-19 21:44:01 -0500

    Create product_template.py

---
+++ models/product_template.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import api, fields, models
+
+
+class ProductLabelFields(models.Model):
+    _inherit = 'product.template'
+
+    lote = fields.Char('Lote')
+    fecha_fab = fields.Date('Fecha de fabricación)
+    fecha_ven = fields.Date('Fecha de vencimiento')
+    peso = fields.Float('Peso kg')
+    vida_util = fields.Char('Vida útil')
+    footer = fields.Char('Pie de página')
