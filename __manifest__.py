76546a41f1c6e288441123de6fec904aaf03ebf9
elmonitor <info@elmonitor.net>
2020-04-19 21:16:47 -0500

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Product Label Fields',
+    'version': '12.0.2.0.0',
+    'development_status': "Mature",
+    'category': 'Product',
+    'summary': "Product Label Fields",
+    'author': 'El Monitor',
+    'website': 'https://elmonitor.net',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+        ],
+    'data': [
+        'views/product_brand_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
